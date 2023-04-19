042d1a327b4a8e7c59d191c56855dc2028001e4f
Emilio Ortiz <ortizemilio.pers@gmail.com>
2023-04-19 07:47:33 -0600

    Safari explorer styles bug fixes

--- .next/static/D4mXlLpO2Ycf8zI1mGoDw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/237-41eec01fa14839e8.js","static/css/ce74e55d22a7bae8.css","static/chunks/pages/index-5d453ffc871ec21e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
